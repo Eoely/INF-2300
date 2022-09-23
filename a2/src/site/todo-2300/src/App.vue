e3d7dfb736972f1d0bcdf7f43177c0b375257655
Eoely <70596863+Eoely@users.noreply.github.com>
2022-09-23 14:25:45 +0400

    stuff done

--- a2/src/site/todo-2300/src/App.vue
+++ a2/src/site/todo-2300/src/App.vue
@@ -2,35 +2,142 @@
 <template>
   <header>
     <h1>'TODO'</h1>
-
   </header>
-  <ul>
-    <li v-for="task in tasks">
-      <input type="checkbox" :value="task.name" :id="task.id"> {{task.name}}
-    </li>
-  </ul>
+
+  <body>
+    <div class="container">
+      <div class="addnew">
+        <input type="button" value="Confirm" @click="appendTask">
+        <input v-model="newTaskName">
+      </div>
+      <div class="rest">
+        <input type="button" value="delete all" @click="deleteAllTasks">
+        <ul style="list-style-type:none">
+          <li v-for="task in tasks" class="todoitem">
+            <input type="button" class="deletebutton" value="X" @click="deleteTask(task.id)">
+            <input type="button" :value="task.done ? 'Undo' : 'Complete'" @click="completeTask(task.id)">
+            <input type="button" value="Edit" @click="updateTaskName(task.id)">
+            <input v-model="task.name" :class="{completedTask: task.done}" @change="testfunc()">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 
-const tasks = ref<any>([]);
-onMounted(() => axios.get('http://127.0.0.1:5000/api/items/').then(response => {tasks.value = response.data["items"]}));
+type Task = {
+  "done": boolean,
+  "id": number,
+  "name": string,
+};
 
+const tasks = ref<Task[]>([]);
+const isAddingNew = ref(false);
+const newTaskName = ref('')
+
+const getTasks = () => axios.get('http://127.0.0.1:5000/api/items/').then(response => tasks.value = response.data["items"]);
+
+onMounted(getTasks);
+
+const showAddTask = () => isAddingNew.value = !isAddingNew.value;
+
+const testfunc = () => console.log('this is a test');
+
+
+const deleteTask = async (id: number) => {
+  await axios.delete(`http://127.0.0.1:5000/api/items/${id}`)
+    .then(response => console.log('delres', response.data))
+    .catch(error => console.log('delerr', error));
+
+  getTasks();
+}
+
+const completeTask = async (id: number) => {
+  let task = tasks.value.find(t => t.id == id);
+
+  if (task) {
+    task.done = !task.done;
+    console.log('task complete', task);
+
+    await axios.put(`http://127.0.0.1:5000/api/items/${id}`, task)
+      .then(response => console.log('putres', response))
+      .catch(error => console.log('puterr', error));
+
+    getTasks();
+  } else {
+    console.error('No task with that id was found');
+  }
+
+}
+
+const updateTaskName = async (id: number) => {
+  let task = tasks.value.find(t => t.id == id);
+  await axios.put(`http://127.0.0.1:5000/api/items/${id}`, task)
+    .then(response => console.log('putres', response))
+    .catch(error => console.log('puterr', error));
+
+  getTasks();
+
+}
+
+const deleteAllTasks = async () => {
+  const reqs = tasks.value.map(task => axios.delete(`http://127.0.0.1:5000/api/items/${task.id}`));
+  //TODO: Handle errors.
+  await axios.all(reqs)
+    .then(data => console.log('deldata', data))
+    .catch(error => console.log('delallerr', error));
+
+  getTasks();
+
+}
+
+const appendTask = async () => {
+  const newTask = {
+    "name": newTaskName.value,
+  }
+
+  await axios.post('http://127.0.0.1:5000/api/items/', newTask)
+    .then(response => console.log(response.data.item))
+    .catch(error => console.error(error))
+
+  newTaskName.value = '';
+  isAddingNew.value = false;
+
+  //Could also just append response to tasks array
+  getTasks();
+};
 
 </script>
 
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
+} */
+
+.completedTask {
+  background-color: green;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.container {
+  border: 0.05em solid red;
+  position: relative;
+  margin: 1rem;
+
+}
+
+.todoitem {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+}
+
+.deletebutton {
+  background-color: red
 }
 
 @media (min-width: 1024px) {
@@ -40,7 +147,7 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
+  /* .logo {
     margin: 0 2rem 0 0;
   }
 
@@ -48,6 +155,6 @@ header {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
+  } */
 }
 </style>
