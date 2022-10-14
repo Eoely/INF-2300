1e00317b4c44b9cf0644bd75742013b3248e2da3
eoely <eos033@uit.no>
2022-10-14 17:14:40 +0200

    added report and comments

--- a2/src/site/src/App.vue
+++ a2/src/site/src/App.vue
@@ -1,21 +1,24 @@
 
 <template>
   <div class="parent">
+
     <head style="display: flex; padding-bottom: 40px;">
-      <h1>TODO INF-2300</h1>    
+      <h1>TODO INF-2300</h1>
       <input type="button" value="Delete all" @click="deleteAllTasks">
     </head>
     <div class="new">
       <input v-model="newTaskName" placeholder="New task" size="50">
       <input type="button" value="+" @click="appendTask">
     </div>
-      <ul>
-        <li v-for="task in tasks" class="todoitem">
-          <input v-model="task.name" :class="{completedTask: task.done}" size="50" @change="updateTaskName(task.id)">
-          <input type="button" value="&#10004" class="button" style="background-color:rgb(142, 211, 142)" @click="completeTask(task.id)">
-          <input type="button" value="&#10006" class="button" style="background-color:rgb(221, 33, 33)" @click="deleteTask(task.id)">
-        </li>
-      </ul>
+    <ul>
+      <li v-for="task in tasks" class="todoitem">
+        <input v-model="task.name" :class="{completedTask: task.done}" size="50" @change="updateTaskName(task.id)">
+        <input type="button" value="&#10004" class="button" style="background-color:rgb(142, 211, 142)"
+          @click="completeTask(task.id)">
+        <input type="button" value="&#10006" class="button" style="background-color:rgb(221, 33, 33)"
+          @click="deleteTask(task.id)">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -33,24 +36,27 @@ const api_url = "http://127.0.0.1:5000/api/items/"
 const tasks = ref<Task[]>([]);
 const newTaskName = ref('')
 
+/*GET request for all tasks, placed in "tasks array"*/
 const getTasks = () => axios.get(api_url)
   .then(response => tasks.value = response.data["items"])
   .catch(error => alertError(error.request.response));
 
 onMounted(getTasks);
 
+/*Default error message, displayed with alert*/
 const alertError = (error: string) => {
   const parsedError = JSON.parse(error);
   alert(`${parsedError.Message}: ${parsedError.Code}\n${parsedError.Description}`)
 }
 
+/*DELETE a task given id*/
 const deleteTask = (id: number) => {
   axios.delete(`${api_url}${id}`)
     .then(getTasks)
     .catch(error => alertError(error.request.response));
 }
 
-
+/*PUT request to complete/undo a task given id*/
 const completeTask = (id: number) => {
   const task = tasks.value.find(t => t.id === id);
   let updatedTask = { ...task };
@@ -64,6 +70,7 @@ const completeTask = (id: number) => {
   }
 }
 
+/*PUT request to update task description, given ID*/
 const updateTaskName = (id: number) => {
   let task = tasks.value.find(t => t.id === id);
 
@@ -72,6 +79,7 @@ const updateTaskName = (id: number) => {
     .catch(error => alertError(error.request.response));
 }
 
+/*Delete all tasks from database*/
 const deleteAllTasks = () => {
   const requests = tasks.value.map(task => axios.delete(`${api_url}${task.id}`));
 
@@ -80,6 +88,7 @@ const deleteAllTasks = () => {
     .catch(error => alertError(error.request.response));
 }
 
+/*POST request to add new task*/
 const appendTask = () => {
   const newTask = {
     "name": newTaskName.value,
@@ -92,7 +101,6 @@ const appendTask = () => {
   newTaskName.value = '';
 };
 </script>
-
 
 <style scoped>
 .parent {
@@ -107,16 +115,19 @@ h1 {
   margin: 0 auto;
 }
 
-ul, li {
+ul,
+li {
   list-style: none;
   padding-left: 0;
   padding-top: 5px;
 }
+
 .completedTask {
   background-color: rgb(142, 211, 142);
 }
 
-input, button {
+input,
+button {
   font-size: inherit;
   padding: 0.3em 0.4em;
   margin: 0.1em 0.2em;
